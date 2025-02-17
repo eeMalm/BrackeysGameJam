d89d69f8a6a51ef3e1c3b542ea55355660f95e24
eeMalm <emanuelmalm07@gmail.com>
2025-02-17 22:33:58 +0100

    Worked some more on the movement code,
     ill fix up the acceleartion tommorow.

--- nothingcangowrong/rat.gd
+++ nothingcangowrong/rat.gd
@@ -2,7 +2,9 @@ extends CharacterBody2D
 
 
 const SPEED : float = 6000.0
+const MAXSPEED : float = 600.0
 var Direction : Vector2
+var Friction : float = 0.0
 
 func _physics_process(delta):
 	Direction = Vector2(0, 0)
@@ -17,7 +19,15 @@ func _physics_process(delta):
 	print(Direction)
 	Direction = Direction.normalized()
 	
-	velocity = Direction * SPEED * delta
+	#fix this tommorow, dosent work well right now.
+	if velocity.length() < MAXSPEED:
+		velocity += Direction * SPEED * delta
+		
+		
+	if Direction.x == 0:
+		velocity.x *= Friction * delta
+	if Direction.y == 0:
+		velocity.y *= Friction * delta
 	
 	
 	move_and_slide()

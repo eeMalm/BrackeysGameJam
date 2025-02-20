fd3b4b9f366ba5f0a019496235673198701dc734
Nodnarb520 <bschatzm000@stu.waukesha.k12.wi.us>
2025-02-20 16:28:12 -0600

    combat adjustments
    -scene now switches to combat when human sees the rat
    -code for broom attack

--- nothingcangowrong/Scripts/broom_attack.gd
+++ nothingcangowrong/Scripts/broom_attack.gd
@@ -1,10 +1,14 @@
 extends CharacterBody2D
 
 
-const SPEED = -30.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = -300.0
+var forward: bool = true
 
 func _physics_process(delta: float) -> void:
-	velocity.x=SPEED
+	if position.x <0:
+		forward=false
+	if forward:
+		velocity.x=SPEED
+	else:
+		velocity.x=-SPEED
 	move_and_slide()

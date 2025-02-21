c606cc81729f549ee938d7ce62064efab4d00fab
eeMalm <emanuelmalm07@gmail.com>
2025-02-21 23:15:03 +0100

    whoops forgot a thing

--- nothingcangowrong/Scripts/camera_2d.gd
+++ nothingcangowrong/Scripts/camera_2d.gd
@@ -9,12 +9,12 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print(cmode)
+
 	if cmode == "combat":
 		#configure the camera here
 		position = Vector2(0, 0)
 		zoom = Vector2(1, 1)
-		print(zoom)
+		
 	elif cmode == "house":
 		position = position.lerp(rat.position, 4.5 * delta)
 		zoom = Vector2(2.75, 2.75)
